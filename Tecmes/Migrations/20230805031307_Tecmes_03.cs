f5372e1cb0d82da48a7f52b655c579d130506525
Tobias Rosa <tobias.rosa2000@gmail.com>
2023-08-05 00:14:53 -0300

    Correctly setup foreign key of production order table

---
+++ Tecmes/Migrations/20230805031307_Tecmes_03.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tecmes.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tecmes_03 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FkSaleOrderTbXProductTb",
+                table: "production_order_tb");
+
+            migrationBuilder.AddForeignKey(
+                name: "FkProductionOrderTbXProductTb",
+                table: "production_order_tb",
+                column: "product_id",
+                principalTable: "product_tb",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FkProductionOrderTbXProductTb",
+                table: "production_order_tb");
+
+            migrationBuilder.AddForeignKey(
+                name: "FkSaleOrderTbXProductTb",
+                table: "production_order_tb",
+                column: "product_id",
+                principalTable: "production_order_tb",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
